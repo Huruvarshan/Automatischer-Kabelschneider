cbfd6b77ba09ba172a405d30ea40fc35718a37a7
Huruvarshan <huruvarshan.sureshkanna@stud.msw.ch>
2024-07-06 12:21:42 +0200

    "BlinK" on IO17

--- Software/AKS_test-program/main/main.c
+++ Software/AKS_test-program/main/main.c
@@ -3,7 +3,16 @@
 #include <freertos/task.h>
 #include "driver/gpio.h"
 
-void app_main(void)
-{
+#define PIN 17
 
+void app_main(void)
+{  
+  gpio_set_direction(PIN, GPIO_MODE_OUTPUT);
+  int isOn = 0;
+  while (true)
+  {
+    isOn = !isOn;
+    gpio_set_level(PIN, isOn);
+    vTaskDelay(pdMS_TO_TICKS(1000));    
+  }
 }
