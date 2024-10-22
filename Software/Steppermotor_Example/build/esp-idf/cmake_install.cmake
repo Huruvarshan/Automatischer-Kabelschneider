857cfa3b06c3ca0ca8949bfa140292434694c067
Huruvarshan <huruvarshan.sureshkanna@stud.msw.ch>
2024-10-22 16:34:08 +0200

    FreeRTOS tasks

--- Software/Steppermotor_Example/build/esp-idf/cmake_install.cmake
+++ Software/Steppermotor_Example/build/esp-idf/cmake_install.cmake
@@ -419,6 +419,11 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
+  include("C:/Users/e1Suresh/Documents/GitHub/Automatischer-Kabelschneider/Software/Steppermotor_Example/build/esp-idf/espressif__led_strip/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
   include("C:/Users/e1Suresh/Documents/GitHub/Automatischer-Kabelschneider/Software/Steppermotor_Example/build/esp-idf/main/cmake_install.cmake")
 endif()
 
--- Software/Steppermotor_Example/main/stepper_motor_example_main.c
+++ Software/Steppermotor_Example/main/stepper_motor_example_main.c
@@ -10,21 +10,30 @@
 #include "driver/gpio.h"
 #include "esp_log.h"
 #include "stepper_motor_encoder.h"
+#include "led_strip.h" 
+#include "esp_random.h"
 
 ///////////////////////////////Change the following configurations according to your board//////////////////////////////
 #define STEP_MOTOR_GPIO_EN       1
 #define STEP_MOTOR_GPIO_DIR      5
 #define STEP_MOTOR_GPIO_STEP     4
-#define STEP_MOTOR_ENABLE_LEVEL  0 // DRV8825 is enabled on low level
+#define STEP_MOTOR_ENABLE_LEVEL  0 
 #define STEP_MOTOR_SPIN_DIR_CLOCKWISE 1
 #define STEP_MOTOR_SPIN_DIR_COUNTERCLOCKWISE STEP_MOTOR_SPIN_DIR_CLOCKWISE 
 
 #define STEP_MOTOR_RESOLUTION_HZ 1000000 // 1MHz resolution
 
-static const char *TAG = "example";
+void stepperMotorTask(void *pvParameters);
+void ledStripTask(void *pvParameters);
 
 void app_main(void)
 {
+    xTaskCreate(stepperMotorTask, "stepperMotorTask", 8192, NULL, 4, NULL);
+    xTaskCreate(ledStripTask, "ledStripTask", 8192, NULL, 5, NULL);    
+}
+
+void stepperMotorTask(void *pvParameters){
+    static const char *TAG = "example";
     ESP_LOGI(TAG, "Initialize EN + DIR GPIO");
     gpio_config_t en_dir_gpio_config = {
         .mode = GPIO_MODE_OUTPUT,
@@ -54,7 +63,7 @@ void app_main(void)
         .resolution = STEP_MOTOR_RESOLUTION_HZ,
         .sample_points = 500,
         .start_freq_hz = 500,
-        .end_freq_hz = 2500,
+        .end_freq_hz = 5000,
     };
     rmt_encoder_handle_t accel_motor_encoder = NULL;
     ESP_ERROR_CHECK(rmt_new_stepper_motor_curve_encoder(&accel_encoder_config, &accel_motor_encoder));
@@ -68,7 +77,7 @@ void app_main(void)
     stepper_motor_curve_encoder_config_t decel_encoder_config = {
         .resolution = STEP_MOTOR_RESOLUTION_HZ,
         .sample_points = 500,
-        .start_freq_hz = 2500,
+        .start_freq_hz = 5000,
         .end_freq_hz = 500,
     };
     rmt_encoder_handle_t decel_motor_encoder = NULL;
@@ -83,8 +92,10 @@ void app_main(void)
     };
 
     const static uint32_t accel_samples = 500;
-    const static uint32_t uniform_speed_hz = 2500;
+    const static uint32_t uniform_speed_hz = 5000;
     const static uint32_t decel_samples = 500;
+
+    uint16_t counter = 0; 
 
     while (1) {
         // acceleration phase
@@ -92,7 +103,8 @@ void app_main(void)
         ESP_ERROR_CHECK(rmt_transmit(motor_chan, accel_motor_encoder, &accel_samples, sizeof(accel_samples), &tx_config));
 
         // uniform phase
-        tx_config.loop_count = 1500;
+        //tx_config.loop_count = counter;
+        tx_config.loop_count = random()%10000;
         ESP_ERROR_CHECK(rmt_transmit(motor_chan, uniform_motor_encoder, &uniform_speed_hz, sizeof(uniform_speed_hz), &tx_config));
 
         // deceleration phase
@@ -101,6 +113,49 @@ void app_main(void)
         // wait all transactions finished
         ESP_ERROR_CHECK(rmt_tx_wait_all_done(motor_chan, -1));
 
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        /*if (counter >= 25000)
+        {
+            counter = 0;
+        } else {
+            counter = counter + 100; 
+        }*/
+        
+        vTaskDelay(pdMS_TO_TICKS(500));        
     }
+}
+
+void ledStripTask(void *pvParameters){
+
+    led_strip_handle_t led_strip;
+
+    /* LED strip initialization with the GPIO and pixels number*/
+    led_strip_config_t strip_config = {
+        .strip_gpio_num = 8, // The GPIO that connected to the LED strip's data line
+        .max_leds = 4, // The number of LEDs in the strip,
+        .led_pixel_format = LED_PIXEL_FORMAT_GRB, // Pixel format of your LED strip
+        .led_model = LED_MODEL_WS2812, // LED strip model
+        .flags.invert_out = false, // whether to invert the output signal (useful when your hardware has a level inverter)
+    };
+
+    led_strip_rmt_config_t rmt_config = {
+    #if ESP_IDF_VERSION < ESP_IDF_VERSION_VAL(5, 0, 0)
+        .rmt_channel = 0,
+    #else
+        .clk_src = RMT_CLK_SRC_DEFAULT, // different clock source can lead to different power consumption
+        .resolution_hz = 10 * 1000 * 1000, // 10MHz
+        .flags.with_dma = false, // whether to enable the DMA feature
+    #endif
+    };
+    ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
+
+    while (1)
+    {
+        led_strip_set_pixel(led_strip, 0, (esp_random()%50), (esp_random()%50), (esp_random()%50)); 
+        /*led_strip_set_pixel(led_strip, 1, 10, 10, 10); 
+        led_strip_set_pixel(led_strip, 2, 10, 10, 10); 
+        led_strip_set_pixel(led_strip, 3, 10, 10, 10); */
+        led_strip_refresh(led_strip);
+
+        vTaskDelay(pdMS_TO_TICKS(250));
+    } 
 }
