7a8ac05d52e8cdad32e158d2c98ed0dc7df9c35a
Huruvarshan <huruvarshan.sureshkanna@stud.msw.ch>
2025-03-25 08:55:01 +0100

    built aks program

---
+++ Software/AKS_Program/main/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/rmt_tx.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "stepper_motor_encoder.h"
+//#include "led_strip.h" 
+#include "esp_random.h"
+#include "math.h"
+#include "esp_wifi.h"
+#include "nvs_flash.h"
+#include "esp_now.h"
+#include "esp_mac.h"
+#include "string.h"
+
+void app_main(void)
+{
+    
+}
